d19f4ca05b2e1069038a80dbecdb28e46f7d34ac
xVanieeeeeeeet <42171095+xVanieeeeeeeet@users.noreply.github.com>
2018-10-08 23:06:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity("Global. || Links","https://twitch.tv//9ivv")
+  client.user.setActivity("Global. || Invite","https://twitch.tv//9ivv")
     console.log('')
     console.log('')
     console.log('╔[════════════════════════════════════════════════════════════════]╗')
